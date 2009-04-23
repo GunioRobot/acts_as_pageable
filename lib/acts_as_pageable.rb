f6dd97f42c3152d9cf5dbda92a5ac35de83bf4e3
Paulo Ahagon <paulo.ahagon@gmail.com>
2009-04-22 23:53:58 -0300

    adding paging in associations

--- lib/acts_as_pageable.rb
+++ lib/acts_as_pageable.rb
@@ -2,5 +2,14 @@ ROOT_PAGEABLE_PATH = File.join(File.dirname(__FILE__), 'acts_as_pageable')
 require File.join(ROOT_PAGEABLE_PATH, 'base')
 require File.join(ROOT_PAGEABLE_PATH, 'page')
 
-require 'active_record' unless defined? ActiveRecord
-ActiveRecord::Base.extend(ActsAsPageable::Base)
+require 'active_record' unless defined? ActiveRecord::Base
+ActiveRecord::Base.extend(ActsAsPageable::Base) unless ActiveRecord::Base.respond_to? :paginate!
+
+returning([ ActiveRecord::Associations::AssociationCollection ]) { |classes|
+  unless ActiveRecord::Associations::HasManyThroughAssociation.superclass == ActiveRecord::Associations::HasManyAssociation
+    classes << ActiveRecord::Associations::HasManyThroughAssociation
+  end
+}.each do |klass|
+  klass.send :include, ActsAsPageable::Base unless klass.respond_to? :paginate! 
+end
+
