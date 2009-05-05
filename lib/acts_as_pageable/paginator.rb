0959d9274d115239a9759a10df96a2bd233f5b5f
Paulo Ahagon <paulo.ahagon@gmail.com>
2009-05-05 00:05:13 -0300

    now we are able to paginate array and default paginate settings

--- lib/acts_as_pageable/paginator.rb
+++ lib/acts_as_pageable/paginator.rb
@@ -2,6 +2,25 @@ module ActsAsPageable
 
   module Paginator
 
+    @@settings = {
+      :page               => 1,
+      :items_per_page     => 1, 
+      :window_offset      => 1, 
+      :max_items_per_page => 200, 
+      :min_items_per_page => 1,
+      :min_window_offset  => 1, 
+      :max_window_offset  => 20
+    }
+
+    def self.settings=(options)
+      @@settings.merge!(options)
+    end
+    
+    def self.settings
+      @@settings
+    end
+
+    attr_accessor :default_paginate_settings
     attr_accessor :paging_named_queries
 
     def method_missing(method_id,*args)
@@ -16,6 +35,35 @@ module ActsAsPageable
       raise ArgumentError.new("undefined key=#{key}") if key.nil? || @paging_named_queries.nil? || 
         @paging_named_queries.empty? || !@paging_named_queries.key?(key) 
       args.nil? ? paginate!(@paging_named_queries[key]) : paginate!(args.merge(@paging_named_queries[key]))
+    end
+
+    def paginate!(options={})
+
+      default_options = @default_paginate_settings.nil? ? @@settings : @default_paginate_settings.merge(@@settings)
+      pag = default_options.merge options
+
+      pag[:items] = case pag[:find].class.to_s
+      when "NilClass"
+        lambda do |offset,limit,opts|
+          entries[offset..limit]
+        end
+      when "Proc"
+        pag[:find]
+      else
+        lambda do |offset,limit,opts|
+          find *pag[:find]
+        end
+      end
+      pag[:total_items] = case pag[:count].class.to_s
+      when "NilClass"
+        lambda{size}
+      when "Proc"
+        pag[:count]
+      else
+        lambda{size}
+      end
+      pag.extend Page
+
     end
     
   end
