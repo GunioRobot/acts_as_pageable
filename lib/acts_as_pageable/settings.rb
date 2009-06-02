593169d43e2b2e6d45f4398c7f4fe37f34253a49
Paulo Ahagon <paulo.ahagon@gmail.com>
2009-06-02 18:49:49 -0300

    I'm changing default settings values

--- lib/acts_as_pageable/settings.rb
+++ lib/acts_as_pageable/settings.rb
@@ -5,15 +5,15 @@ module ActsAsPageable
     @@settings = {
       :page               => 1,
       :items_per_page     => 10, 
-      :window_offset      => 2, 
+      :window_offset      => 5, 
       :max_items_per_page => 200, 
       :min_items_per_page => 5,
-      :min_window_offset  => 1, 
-      :max_window_offset  => 20
+      :max_window_offset  => 10,
+      :min_window_offset  => 5 
     }
 
-    def self.settings=(options)
-      @@settings.merge!(options)
+    def self.settings=(settings)
+      @@settings.merge!(settings)
     end
     
     def self.settings
